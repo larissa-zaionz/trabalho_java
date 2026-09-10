1c5671b6d24ebcaa244fa007bc148d4c90814291
larissa-zaionz <proglarissa@gmail.com>
2026-09-10 16:34:15 -0300

    mudancas de final e aula

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
 const song = document.getElementById('audio');
+const capaMusic = document.getElementById('capa');
 const play = document.getElementById('play');
 
-songName.innerText = 'Chlorine';
+const chlorine = {
+    songName : 'Chlorine',
+    artist : 'Twenty one Pilots',
+    file : 'twentyonepilots'
+};
+const justinBieber = {
+    songName : 'Sorry',
+    artist : 'Justin Bieber',
+    file : 'justinbieber'
+};
+const babyDoll = {
+    songName : 'BabyDoll',
+    artist : 'Dominic Fike',
+    file : 'babydoll'
+};
+
 let isPlayIng = false;
+const playlist = [chlorine, justinBieber, babyDoll];
+let index = 0;
 
 function playSong(){
     play.querySelector('.bi').classList.remove('bi-play-circle-fill');
@@ -28,4 +47,14 @@ function playPauseDecider(){
     }
 }
 
+function carregarInformacoes(){
+    capaMusic.src = `imagens/${playlist[index].file}`;
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerText = playlist[index].songName;
+    bandName.innerText = playlist[index].artist;
+}
+
+carregarInformacoes();
 play.addEventListener('click', playPauseDecider);
+011 100 101
+  3   8    5
